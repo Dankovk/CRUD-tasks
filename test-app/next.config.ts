1fa6f2354533ac2473c968655393c5ffe4d2e4fd
Dankov Alex <dankov.kiev@gmail.com>
2025-08-29 11:20:14 +0300

    Update Dockerfile to include Drizzle directory and modify CMD for database migration before starting the application. Adjust Next.js configuration to remove standalone output setting.

--- test-app/next.config.ts
+++ test-app/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   experimental: {
     reactCompiler: true,
-  }
+  },
 };
 
 export default nextConfig;
